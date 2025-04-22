a547e7e778a38e10690026baa379d21e609cb82b
sunamp <sunamp@gmail.com>
2025-04-22 18:29:40 +1000

    Add files via upload
    Drink cans template

---
+++ drink_cans.py
@@ -0,0 +1,16 @@
+"""
+Student Name:
+Student ID:
+
+"""
+
+# read price of a can using the correct datatype
+
+
+# read number of cans using the correct datatype
+
+
+# calculate final total with the service charge
+
+
+# print final total with formatting
